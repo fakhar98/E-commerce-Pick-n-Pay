c91f89c7b05613b84d1957cd80e5aa553a6714a0
fakhar98 <fakharkaramat9@gmail.com>
2025-06-26 23:49:35 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/E-commerce-Pick-n-Pay/', // <-- Add this line
+  base: '/E-commerce-Pick-n-Pay/',
   plugins: [react()],
 });
